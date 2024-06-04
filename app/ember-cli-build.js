50cef60fa463a8515e9787c37b2fd528f41d0afc
Manish Agrawal <manish.agrawal@freshworks.com>
2024-06-04 11:42:50 +0530

    init

---
+++ app/ember-cli-build.js
@@ -0,0 +1,28 @@
+'use strict'; 
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  let app = new EmberApp(defaults, {
+    autoImport: {
+      webpack: {
+        module: {
+          rules: [
+            {
+              test: /some-react-lib\/.*\.jsx/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: [['@babel/preset-react', { runtime: 'automatic' }]],
+                },
+              },
+            },
+          ],
+        },
+      },
+    }
+  });
+
+  return app.toTree();
+};
+ 
